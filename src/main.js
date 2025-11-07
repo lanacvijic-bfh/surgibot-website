c8681139c947a281bd327e529912d708be3fc86c
Lana <lana@MacBook-Pro-von-Administrator.local>
2025-11-07 15:24:54 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './style.css'
+
+document.querySelector('SurgiBot').innerHTML = `
+  <div class="min-h-screen flex flex-col">
+    <!-- your navbar + hero + sections here, using Tailwind classes -->
+  </div>
+`;
