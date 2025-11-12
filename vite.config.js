fe79615c4c82563457f3201234cc6685c2549840
Lana <lana@MacBook-Pro-von-Administrator.local>
2025-11-12 18:16:42 +0100

    new changes on the website

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [tailwind()],
 });
