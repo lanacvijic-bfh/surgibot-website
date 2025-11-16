c273fc7304b7410440414f5746cdd72f4c842ce5
Lana <lana@MacBook-Pro-von-Administrator.local>
2025-11-16 12:59:06 +0100

    make deployment work

--- public/vignette-view.js
+++ public/vignette-view.js
@@ -1,5 +1,5 @@
-import { vignettes } from "./src/lib/vignettes.js";
-import { buildPatientSystemPrompt } from "./src/lib/patient-prompt.js";
+import { vignettes } from "./lib/vignettes.js";
+import { buildPatientSystemPrompt } from "./lib/patient-prompt.js";
 // ^ IMPORTANT: file name has a dash, so import it exactly like this
 
 function getVignetteIdFromUrl() {
